6f78a298e134cfd89f3362fbd692fe75d4e6508c
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2024-03-10 21:03:18 +0000

    adds byte count as in the proposed exercise

--- wc/main_test.go
+++ wc/main_test.go
@@ -5,11 +5,22 @@ import (
 	"testing"
 )
 
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("abc")
+
+	expected := 3
+	actual := count(b, false, true)
+
+	if actual != expected {
+		t.Errorf("expected %d, got %d instead.\n", expected, actual)
+	}
+}
+
 func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
 
 	expected := 3
-	actual := count(b, true)
+	actual := count(b, true, false)
 
 	if actual != expected {
 		t.Errorf("expected %d, got %d instead.\n", expected, actual)
@@ -19,7 +30,7 @@ func TestCountLines(t *testing.T) {
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	expected := 4
-	actual := count(b, false)
+	actual := count(b, false, false)
 
 	if actual != expected {
 		t.Errorf("expected %d, got %d instead.\n", expected, actual)
