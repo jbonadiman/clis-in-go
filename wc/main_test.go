0a9a9bbaa2bcce05b2386d299cae13c912658e29
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2024-03-10 20:52:17 +0000

    adds command-line option for counting lines

--- wc/main_test.go
+++ wc/main_test.go
@@ -5,10 +5,21 @@ import (
 	"testing"
 )
 
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+
+	expected := 3
+	actual := count(b, true)
+
+	if actual != expected {
+		t.Errorf("expected %d, got %d instead.\n", expected, actual)
+	}
+}
+
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	expected := 4
-	actual := count(b)
+	actual := count(b, false)
 
 	if actual != expected {
 		t.Errorf("expected %d, got %d instead.\n", expected, actual)
