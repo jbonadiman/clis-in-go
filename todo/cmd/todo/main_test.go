979686338609040bf4ba2e444ea24bf0666c1222
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2024-03-11 21:49:08 +0000

    replaces the cli interface package to flag; adds test for the complete command, as suggested

--- todo/cmd/todo/main_test.go
+++ todo/cmd/todo/main_test.go
@@ -6,7 +6,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"testing"
 )
 
@@ -49,7 +48,7 @@ func TestTodoCLI(t *testing.T) {
 
 	cmdPath := filepath.Join(dir, binName)
 	t.Run("AddNewTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		cmd := exec.Command(cmdPath, "-task", task)
 
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
@@ -57,13 +56,31 @@ func TestTodoCLI(t *testing.T) {
 	})
 
 	t.Run("ListTasks", func(t *testing.T) {
-		cmd := exec.Command(cmdPath)
+		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Fatal(err)
 		}
 
 		expected := task + "\n"
+		if expected != string(out) {
+			t.Errorf("expected %q, got %q instead\n", expected, string(out))
+		}
+	})
+
+	t.Run("CompleteTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-complete", "1")
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+
+		cmd = exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := ""
 		if expected != string(out) {
 			t.Errorf("expected %q, got %q instead\n", expected, string(out))
 		}
