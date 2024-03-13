570eb00cfb82bfe4bdc7652b6852fea13b741a82
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2024-03-13 21:41:58 +0000

    adds file preview opened automatically

--- mdp/main.go
+++ mdp/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/yuin/goldmark"
@@ -33,19 +35,20 @@ const (
 
 func main() {
 	filename := flag.String("file", "", "markdown file to preview")
+	skipPreview := flag.Bool("s", false, "skip auto-preview")
 	flag.Parse()
 
 	if *filename == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
-	if err := run(*filename, os.Stdout); err != nil {
+	if err := run(*filename, os.Stdout, *skipPreview); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(filename string, out io.Writer) error {
+func run(filename string, out io.Writer, skipPreview bool) error {
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
@@ -64,7 +67,15 @@ func run(filename string, out io.Writer) error {
 	outName := temp.Name()
 	fmt.Fprintln(out, outName)
 
-	return saveHTML(outName, htmlData)
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	return preview(outName)
 }
 
 func parseContent(input []byte) []byte {
@@ -93,4 +104,29 @@ func parseContent(input []byte) []byte {
 
 func saveHTML(outFname string, data []byte) error {
 	return os.WriteFile(outFname, data, 0644)
+}
+
+func preview(fname string) error {
+	cName := ""
+	cParams := []string{}
+
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("os not supported")
+	}
+
+	cParams = append(cParams, fname)
+	cPath, err := exec.LookPath(cName)
+	if err != nil {
+		return err
+	}
+
+	return exec.Command(cPath, cParams...).Run()
 }
