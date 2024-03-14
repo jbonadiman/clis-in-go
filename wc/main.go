2db154d5352ad79f13cb499ca358e9dbe25bbd84
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2024-03-14 18:58:20 +0000

    adds support for reading multiple files

--- wc/main.go
+++ wc/main.go
@@ -6,28 +6,62 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
 	bytes := flag.Bool("b", false, "Count bytes")
+	fileNames := flag.String("file", "", "provide a file name or multiple file names separated by comma")
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines, *bytes))
-}
+	target := []io.Reader{os.Stdin}
 
-func count(r io.Reader, countLines bool, countBytes bool) int {
-	scanner := bufio.NewScanner(r)
+	if *fileNames != "" {
+		var err error
 
-	if countBytes {
-		scanner.Split(bufio.ScanBytes)
-	} else if !countLines {
-		scanner.Split(bufio.ScanWords)
+		target, err = parseFiles(*fileNames)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 
+	fmt.Println(count(*lines, *bytes, target...))
+}
+
+func parseFiles(fileNames string) ([]io.Reader, error) {
+	parsedFileNames := strings.Split(fileNames, ",")
+
+	files := []io.Reader{}
+
+	for _, name := range parsedFileNames {
+		file, err := os.Open(name)
+		if err != nil {
+			return nil, err
+		}
+
+		files = append(files, file)
+	}
+
+	return files, nil
+}
+
+func count(countLines bool, countBytes bool, r ...io.Reader) int {
 	wc := 0
-	for scanner.Scan() {
-		wc++
+
+	for _, reader := range r {
+		scanner := bufio.NewScanner(reader)
+
+		if countBytes {
+			scanner.Split(bufio.ScanBytes)
+		} else if !countLines {
+			scanner.Split(bufio.ScanWords)
+		}
+
+		for scanner.Scan() {
+			wc++
+		}
 	}
 
 	return wc
--- wc/main_test.go
+++ wc/main_test.go
@@ -9,7 +9,7 @@ func TestCountBytes(t *testing.T) {
 	b := bytes.NewBufferString("abc")
 
 	expected := 3
-	actual := count(b, false, true)
+	actual := count(false, true, b)
 
 	if actual != expected {
 		t.Errorf("expected %d, got %d instead.\n", expected, actual)
@@ -20,7 +20,7 @@ func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
 
 	expected := 3
-	actual := count(b, true, false)
+	actual := count(true, false, b)
 
 	if actual != expected {
 		t.Errorf("expected %d, got %d instead.\n", expected, actual)
@@ -30,7 +30,7 @@ func TestCountLines(t *testing.T) {
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	expected := 4
-	actual := count(b, false, false)
+	actual := count(false, false, b)
 
 	if actual != expected {
 		t.Errorf("expected %d, got %d instead.\n", expected, actual)
