d6c07d9a21a99eaf3fbe9f3e81e059d2ae944f9f
João Vitor Bonadiman <18357636+jbonadiman@users.noreply.github.com>
2024-03-15 11:09:53 +0000

    adds initial implementation for walk

---
+++ walk/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type config struct {
+	ext  string
+	size int64
+	list bool
+}
+
+func main() {
+	root := flag.String("root", ".", "root directory to start")
+	list := flag.Bool("list", false, "list files only")
+	ext := flag.String("ext", "", "file extension to filter out")
+	size := flag.Int64("size", 0, "minimum file size")
+	flag.Parse()
+
+	c := config{
+		ext:  *ext,
+		size: *size,
+		list: *list,
+	}
+
+	if err := run(*root, os.Stdout, c); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(root string, out io.Writer, cfg config) error {
+	return filepath.Walk(root,
+		func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if filterOut(path, cfg.ext, cfg.size, info) {
+				return nil
+			}
+
+			if cfg.list {
+				return listFile(path, out)
+			}
+
+			return listFile(path, out)
+		})
+}
